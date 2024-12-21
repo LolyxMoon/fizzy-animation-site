d10ffe47e4486e5b7b7fdc231d27bb3807fe8e91
Nanae-j <nanae9577@gmail.com>
2024-12-22 00:40:34 +0900

    SodaCanの作成・表示まで

--- fizzy-nanae/src/components/ViewCanvas.tsx
+++ fizzy-nanae/src/components/ViewCanvas.tsx
@@ -1,16 +1,32 @@
 "use client";
 
 import { Canvas } from "@react-three/fiber";
+import { Environment } from "@react-three/drei";
+import FloatingCan from "@/components/FloatingCan";
 
 type Props = {};
 
 export default function ViewCanvas({}: Props) {
   return (
-    <Canvas>
-      <mesh>
-        <boxGeometry />
-        <meshStandardMaterial />
-      </mesh>
+    <Canvas
+      style={{
+        position: "fixed",
+        top: 0,
+        left: "50%",
+        transform: "translate(-50%)",
+        overflow: "hidden",
+        pointerEvents: "none",
+        zIndex: 30,
+      }}
+      shadows
+      dpr={[1, 1.5]}
+      gl={{ antialias: true }}
+      camera={{
+        fov: 30,
+      }}
+    >
+      <FloatingCan />
+      <Environment files={"/hdr/lobby.hdr"} environmentIntensity={1.5} />
     </Canvas>
   );
 }

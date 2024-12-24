ae3a29e24bc38a8157859ef504d3ffe306de30a6
Nanae-j <nanae9577@gmail.com>
2024-12-24 11:43:55 +0900

    Loader追加、sliceのシュミレーションページ修正

--- fizzy-nanae/src/components/ViewCanvas.tsx
+++ fizzy-nanae/src/components/ViewCanvas.tsx
@@ -1,32 +1,36 @@
 "use client";
 
-import { View } from "@react-three/drei";
+import { Loader, View } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
-import { Perf } from "r3f-perf";
+import { Suspense } from "react";
 
 type Props = {};
 
 export default function ViewCanvas({}: Props) {
   return (
-    <Canvas
-      style={{
-        position: "fixed",
-        top: 0,
-        left: "50%",
-        transform: "translate(-50%)",
-        overflow: "hidden",
-        pointerEvents: "none",
-        zIndex: 30,
-      }}
-      shadows
-      dpr={[1, 1.5]}
-      gl={{ antialias: true }}
-      camera={{
-        fov: 30,
-      }}
-    >
-      <View.Port />
-      <Perf />
-    </Canvas>
+    <>
+      <Canvas
+        style={{
+          position: "fixed",
+          top: 0,
+          left: "50%",
+          transform: "translate(-50%)",
+          overflow: "hidden",
+          pointerEvents: "none",
+          zIndex: 30,
+        }}
+        shadows
+        dpr={[1, 1.5]}
+        gl={{ antialias: true }}
+        camera={{
+          fov: 30,
+        }}
+      >
+        <Suspense fallback={null}>
+          <View.Port />
+        </Suspense>
+      </Canvas>
+      <Loader />
+    </>
   );
 }

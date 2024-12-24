86899765acfe0c9778a1387cb726bc021378a2bf
Nanae-j <nanae9577@gmail.com>
2024-12-24 20:52:54 +0900

    alternatingスライス完成
    window幅を変えた時の挙動未対応

--- fizzy-nanae/src/slices/AlternatingText/Scene.tsx
+++ fizzy-nanae/src/slices/AlternatingText/Scene.tsx
@@ -8,11 +8,13 @@ import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useGSAP } from "@gsap/react";
 import { useEffect, useLayoutEffect } from "react";
+import { useMediaQuery } from "@/hooks/useMediaQuery";
 
 type Props = {};
 
 export default function Scene({}: Props) {
   const canRef = useRef<Group>(null);
+  const isDesktop = useMediaQuery("(min-width: 768px)", true);
 
   const bgColors = ["#FFA6B5", "#E9CFF6", "#CBEF9A"];
 
@@ -23,49 +25,60 @@ export default function Scene({}: Props) {
     gsap.registerPlugin(useGSAP, ScrollTrigger);
   });
 
-  useGSAP(() => {
-    if (!canRef.current) return;
-
-    const sections = gsap.utils.toArray(".alternating-section");
-
-    const scrollTl = gsap.timeline({
-      scrollTrigger: {
-        trigger: ".alternating-text-view",
-        endTrigger: ".alternating-text-container",
-        pin: true,
-        start: "top top",
-        end: "bottom bottom",
-        scrub: true,
-      },
-    });
-
-    sections.forEach((_, index) => {
+  useGSAP(
+    () => {
       if (!canRef.current) return;
-      if (index === 0) return;
 
-      const isOdd = index % 2 !== 0;
-      scrollTl
-        .to(canRef.current.position, {
-          x: isOdd ? "-1" : "1",
-          ease: "circ.inOut",
-          delay: 0.5,
-        })
-        .to(
-          canRef.current.rotation,
-          {
-            y: isOdd ? ".4" : "-.4",
-            ease: "back.inOut",
-          },
-          "<",
-        )
-        .to(".alternating-text-container", {
-          backgroundColor: gsap.utils.wrap(bgColors, index),
-        });
-    });
-  });
+      const sections = gsap.utils.toArray(".alternating-section");
+
+      const scrollTl = gsap.timeline({
+        scrollTrigger: {
+          trigger: ".alternating-text-view",
+          endTrigger: ".alternating-text-container",
+          pin: true,
+          start: "top top",
+          end: "bottom bottom",
+          scrub: true,
+        },
+      });
+
+      sections.forEach((_, index) => {
+        if (!canRef.current) return;
+        if (index === 0) return;
+
+        const isOdd = index % 2 !== 0;
+
+        const xPosition = isDesktop ? (isOdd ? "-1" : "1") : 0;
+        const yPosition = isDesktop ? (isOdd ? ".4" : "-.4") : 0;
+
+        scrollTl
+          .to(canRef.current.position, {
+            x: xPosition,
+            ease: "circ.inOut",
+            delay: 0.5,
+          })
+          .to(
+            canRef.current.rotation,
+            {
+              y: yPosition,
+              ease: "back.inOut",
+            },
+            "<",
+          )
+          .to(".alternating-text-container", {
+            backgroundColor: gsap.utils.wrap(bgColors, index),
+          });
+      });
+    },
+    { dependencies: [isDesktop] },
+  );
 
   return (
-    <group ref={canRef} position-x={1} rotation-y={-0.3}>
+    <group
+      ref={canRef}
+      position-x={isDesktop ? 1 : 0}
+      rotation-y={isDesktop ? -0.3 : 0}
+    >
       <FloatingCan flavor="strawberryLemonade" />
       <Environment files={"/hdr/lobby.hdr"} environmentIntensity={1.5} />
     </group>
--- fizzy-nanae/src/slices/AlternatingText/index.tsx
+++ fizzy-nanae/src/slices/AlternatingText/index.tsx
@@ -8,8 +8,8 @@ import {
   SliceComponentProps,
 } from "@prismicio/react";
 import { View } from "@react-three/drei";
-import { div } from "three/tsl";
 import Scene from "./Scene";
+import clsx from "clsx";
 
 /**
  * Props for `AlternatingText`.
@@ -28,7 +28,7 @@ const AlternatingText = ({ slice }: AlternatingTextProps): JSX.Element => {
       className="alternating-text-container relative bg-yellow-300 text-sky-950"
     >
       <div>
-        <div className="relative grid">
+        <div className="relative z-[100] grid">
           <View className="alternating-text-view absolute left-0 top-0 h-screen w-full">
             <Scene />
           </View>
@@ -38,7 +38,12 @@ const AlternatingText = ({ slice }: AlternatingTextProps): JSX.Element => {
               key={asText(item.heading)}
               className="alternating-section grid h-screen place-items-center gap-x-12 md:grid-cols-2"
             >
-              <div className={index % 2 === 0 ? "col-start-1" : "col-start-2"}>
+              <div
+                className={clsx(
+                  index % 2 === 0 ? "col-start-1" : "md:col-start-2",
+                  "rounded-lg p-4 backdrop-blur-lg max-md:bg-white/30",
+                )}
+              >
                 <h2 className="text-balance text-6xl font-bold">
                   <PrismicText field={item.heading} />
                 </h2>
